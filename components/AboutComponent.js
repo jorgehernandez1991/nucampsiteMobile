3b6e7987d77316da942767979c5a22b5f286afde
Jorge Hernandez <jorgehernandez4791@gmail.com>
2021-03-09 16:54:07 -0800

    "Activity Indicator".

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,7 +4,7 @@ import {Card, ListItem } from 'react-native-elements';
 import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -33,17 +33,40 @@ static navigationOptions = {
     title: 'About us'
 }
     
-    render() {
+render() {
+    const renderPartner = ({item}) => {
+        return (
+            <ListItem
+                title={item.name}
+                subtitle={item.description}
+                leftAvatar={{source: {uri: baseUrl + item.image}}}
+            />
+        );
+    };
 
-        const renderPartner = ({item}) => {
-            return (
-                <ListItem
-                    title={item.name}
-                    subtitle={item.description}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
-                />
-            );
-        };
+    if (this.props.partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card
+                    title='Community Partners'>
+                    <Loading />
+                </Card>
+            </ScrollView>
+        );
+    }
+    if (this.props.partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card
+                    title='Community Partners'>
+                    <Text>{this.props.partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+
 
     return (
    
