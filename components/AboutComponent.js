76f1d23c6b7fd2f5a0b74fbdb55b982c985de75e
Jorge Hernandez <jorgehernandez4791@gmail.com>
2021-03-11 14:15:43 -0800

    "Forms".

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import {ScrollView,Text} from 'react-native';
-import {Card, ListItem } from 'react-native-elements';
-import { FlatList } from 'react-native';
+import { ScrollView, Text, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
@@ -11,86 +10,64 @@ const mapStateToProps = state => {
         partners: state.partners
     };
 };
-
-
-function Mission(){
+function Mission() {
     return (
-        
-            <Card title="Our Mission">
-                <Text>
+        <Card title='Our Mission'>
+            <Text style={{ margin: 10 }}>
                 We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
-                </Text>
-            </Card>
-        
-    )
+            </Text>
+        </Card>
+    );
 }
-
-
 class About extends Component {
-   
-    
-static navigationOptions = {
-    title: 'About us'
-}
-    
-render() {
-    const renderPartner = ({item}) => {
-        return (
-            <ListItem
-                title={item.name}
-                subtitle={item.description}
-                leftAvatar={{source: {uri: baseUrl + item.image}}}
-            />
-        );
-    };
-
-    if (this.props.partners.isLoading) {
+    static navigationOptions = {
+        title: 'About Us'
+    }
+    render() {
+        const renderPartner = ({ item }) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
+                />
+            );
+        };
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
         return (
             <ScrollView>
                 <Mission />
                 <Card
-                    title='Community Partners'>
-                    <Loading />
-                </Card>
-            </ScrollView>
-        );
-    }
-    if (this.props.partners.errMess) {
-        return (
-            <ScrollView>
-                <Mission />
-                <Card
-                    title='Community Partners'>
-                    <Text>{this.props.partners.errMess}</Text>
-                </Card>
-            </ScrollView>
-        );
-    }
-
-
-    return (
-   
-        <ScrollView>
-        <Mission/>
-        <Card title = "Community Partners ">
-             
-        <FlatList 
+                    title="Community Partners">
+                     <FlatList 
                         data={this.props.partners.partners}
                         renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}
                     />
-
-             </Card>
-
-        </ScrollView>
-        
-    );
+                </Card>
+            </ScrollView>
+        );
+    }
 }
-
-
-
-}
-
-
-
-export default connect(mapStateToProps)(About);
+export default connect (mapStateToProps) (About);
